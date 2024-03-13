29091447be5ba51618044bbc1027c42b4fca8500
William Crawford <crawfowa@gmail.com>
2024-03-12 22:06:48 -0700

    Keep filling model out, started forward, needs reveision

--- src/model.py
+++ src/model.py
@@ -11,10 +11,34 @@ class GPT(nn.Module):
         assert config.block_count is not None
         
 
-        self.transformer = nn.ModuleDict(dict(
-        tkn_embd = nn.Embedding(config.token_count, config.embedding_dim),
-        pos_embd = nn.Embedding(config.block_length, config.embedding_dim),
-        dropout_layer = nn.Dropout(config.dropout)
-        for i in range(0, config.block_count): # nope gotta use a list comprehension for this
+        self.transformer = nn.ModuleDict(
+            dict(
+                tkn_embd = nn.Embedding(config.token_count, config.embedding_dim),
+                pos_embd = nn.Embedding(config.block_length, config.embedding_dim),
+                drpout = nn.Dropout(config.dropout),
+                blocks = [Block for _ in range(config.block_count),
+                lyr_nrm = LayerNorm(config.embedding_dim, config,bias)
+            )
         )
+        self.lm_head = LMHead(config.embedding_dim, config.token_count, bias=False)
+
+    def forward(self, input_block, trgt_tkn):
+        device = input_block.device()
+        b, h = input_block.dims()
+        # do a check here to make sure these params aren't too big for our model
+        pos_seq = torch.arrange(0, h)
+
+        # transformer forward here
+        embdgs_t = self.transformer.tkn_embd(input_block)
+        embdgs_p = self.transformer.pos_embd(pos_seq)
+        # do dropout (on what?)
+        for block in self.transfoerm.blocks:
+            out = block(out)
+        out = self.tranformer.lyr_nrm(out)
+
+        logit = self.lm_head(out)
+        
+        loss = nn.CrossEntropyLoss_(logit, trgt)
+
+
 
