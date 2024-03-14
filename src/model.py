0ff6d1928e92fc2f637c566f0f63c0c651d64743
William Crawford <crawfowa@gmail.com>
2024-03-13 22:09:55 -0700

    More fleshing out of model.py

--- src/model.py
+++ src/model.py
@@ -1,8 +1,20 @@
+"""
+Based upon the nanoGPT and miniGPT implementatinos of Andrey Karpathy
+"""
 from torch import nn
+
+class Block(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        
+
+class LayerNorm(nn.Module):
+    def __init__(self, config):
+        super().__init__()
 
 class GPT(nn.Module):
     def __init__(self, config):
-        super().__init()
+        super().__init__()
         # check here to make sure our relevant config stats are valid
         assert config.token_count is not None
         assert config.embedding_dim is not None
@@ -16,11 +28,11 @@ class GPT(nn.Module):
                 tkn_embd = nn.Embedding(config.token_count, config.embedding_dim),
                 pos_embd = nn.Embedding(config.block_length, config.embedding_dim),
                 drpout = nn.Dropout(config.dropout),
-                blocks = [Block for _ in range(config.block_count),
+                blocks = nn.ModuleList([Block(config) for _ in range(config.block_count)),
                 lyr_nrm = LayerNorm(config.embedding_dim, config,bias)
             )
         )
-        self.lm_head = LMHead(config.embedding_dim, config.token_count, bias=False)
+        self.lm_head = nn.Linear(config.embedding_dim, config.token_count, bias=False)
 
     def forward(self, input_block, trgt_tkn):
         device = input_block.device()
